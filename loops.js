2820ee617c49ece76aa33105b807f36d12310a17
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-04 03:47:52 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,4 @@
 //"I am ___ strange loop"
-var strangeLoop =[]
 for(let i=0;i<25;i++){
-  strangeLoop = [strangeLoop, ("I am "+i+" strange loops.")]
+  console.log("I am "+i+" strange loops.")
 }
-return strangeLoop
