901dfab7570d5de05e82ffd39e963ae706886fd5
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-04 03:57:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,12 @@
 //"I am ___ strange loop"
 function forLoop (array){
   for(let i=0;i<25;i++){
-  array.push("I am "+i+" strange loop.")
+    if i=1 then {
+      array.push("I am "+i+"strange loop")
+    }
+    else {
+      array.push("I am "+i+" strange loop.")
+    }
   }
   return array
 }
